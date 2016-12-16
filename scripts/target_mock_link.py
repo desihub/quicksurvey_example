bdcd1d575568f3e3a56149ca7d3280684fdbf88f
Jaime Forero-Romero <j.e.forero.romero@gmail.com>
2016-12-16 10:46:02 -0800

    example to recover the velocity from the MWS mock

--- scripts/target_mock_link.py
+++ scripts/target_mock_link.py
@@ -4,14 +4,11 @@ import argparse
 import yaml
 
 from desitarget.mock.io import decode_rownum_filenum
-from desitarget.mock.io import load_all_mocks
 from astropy.table import Table
-import desitarget.mock.io as dtmockio
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--config','-c',default='input.yaml')
-parser.add_argument("--targettype", "-T", help="Target type (i.e. LRG, QSO, ELG)", type=str, default="MWS")
 parser.add_argument("--input_dir", "-I", help="Path to the truth.fits and target.fits files", type=str, default="./")
 args = parser.parse_args()
 
@@ -20,19 +17,14 @@ with open(args.config,'r') as pfile:
     params = yaml.load(pfile)
 
 
-#remove information from sources different than args.targettype
-source_name = args.targettype
-keys = []
-for k in params['sources'].keys():
-    keys.append(k)
+#defines the target and variable to recover from the mock
+source_name = 'MWS_MAIN'
+variable_to_recover = 'vX'
 
-for k in keys:
-    if k!=source_name:
-        params['sources'].pop(k)
-
-# load the mocks
-mock_dict = load_all_mocks(params)
-
+# load the map_id_filename
+map_id_filename = np.loadtxt(os.path.join(args.input_dir,'map_id_filename.txt'), 
+                             dtype={'names': ('SOURCENAME', 'FILEID', 'FILENAME'),
+                                   'formats': ('S10', 'i4', 'S256')})
 # load truth
 truth_table = Table.read(os.path.join(args.input_dir, 'truth.fits'))
 print('loaded {} truth items'.format(len(truth_table)))
@@ -41,32 +33,32 @@ print('loaded {} truth items'.format(len(truth_table)))
 target_table = Table.read(os.path.join(args.input_dir, 'targets.fits'))
 print('loaded {} target items'.format(len(target_table)))
 
-
-
+# decode rowid and fileid for the targets of interest
 ii = truth_table['SOURCETYPE']==source_name
 rowid, fileid = decode_rownum_filenum(truth_table['MOCKID'][ii])
 
+# get the fileids to be read
 fileid_to_read = np.array(list(set(fileid)))
-print(fileid_to_read)
+print('fileid to be read {}'.format(fileid_to_read))
 
+# prepare the arrays to save the variable to match
 n = np.count_nonzero(ii)
-mock_ra = np.empty((0))
-mock_dec = np.empty((0))
+to_recover = np.empty((0))
 
-dict_format_func = {'galaxia':'_load_mock_mws_file'}
 
 for i in fileid_to_read:
-    filename =  mock_dict[source_name]['FILES'][i]
-    source_format = params['sources'][source_name]['format']
+    ii = (map_id_filename['SOURCENAME']==source_name.encode()) & (map_id_filename['FILEID']==i)
 
-    func_name = dict_format_func[source_format]
-    func = getattr(dtmockio, func_name)
-    result = func(filename)
+    filename = map_id_filename['FILENAME'][ii]
+    filename = filename[0].decode()
+    print('reading {}'.format(filename))
+    result = Table.read(filename)
 
     rows_to_get = rowid[fileid==i]
-    mock_ra = np.append(mock_ra, result['RA'][rows_to_get])
-
-    print(result)
+    to_recover = np.append(to_recover, result[variable_to_recover][rows_to_get])
 
 
-from IPython import embed; embed()
+
+
+
+
