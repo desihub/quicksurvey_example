687478fab618d61cafddc87d73cb96203ee8f8f2
Jaime Forero-Romero <j.e.forero.romero@gmail.com>
2017-08-01 05:46:36 -0700

    added surveysim script

---
+++ survey/surveysim_HA.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#############################################################################
+# Example automation for cycling between surveyplan and surveysim using
+# a greedy scheduling algorithm for the next tile selector. Since the
+# greedy scheduler does not use hour-angle assignments, surveyplan is
+# relatively fast since no hour-angle optimization is required. The total
+# time to run a 5-year survey with this script is about 45 minutes.
+#############################################################################
+
+export DESISURVEY='/global/cscratch1/sd/forero/quicksurvey_example/survey'
+
+PLAN_ARGS='--verbose --nopt 0'
+SIM_ARGS='--verbose --seed 123 --strategy HA+fallback --plan plan.fits'
+
+surveyplan --create ${PLAN_ARGS}
+surveysim ${SIM_ARGS}
+
+while :
+do
+    (surveyplan ${PLAN_ARGS}) || break
+    (surveysim --resume ${SIM_ARGS}) || break
+done
