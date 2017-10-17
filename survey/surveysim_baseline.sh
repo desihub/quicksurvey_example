a1adecb5382516079604935571d5772494321543
Jaime Forero-Romero <j.e.forero.romero@gmail.com>
2017-10-16 20:55:57 -0700

    baseline

--- survey/surveysim_baseline.sh
+++ survey/surveysim_baseline.sh
@@ -4,12 +4,10 @@
 # Note that this is one random realization of the observing conditions.
 # Change the random seed for a different realization.
 # This will take ~4 hours to run and writes ~1.7G to $DESISURVEY_OUTPUT.
-# Remove the --scores option to reduce the output size to ~50M.
 #############################################################################
-export DESISURVEY=${SCRATCH}'/quicksurvey_example/survey'
 export DESISURVEY_OUTPUT=${SCRATCH}'/quicksurvey_example/survey'
 PLAN_ARGS='--verbose --fa-delay 1m'
-SIM_ARGS='--verbose --scores --seed 123 --strategy HA+fallback'
+SIM_ARGS='--verbose --seed 123 --strategy HA+fallback'
 
 surveyinit --verbose
 surveyplan --create ${PLAN_ARGS}
